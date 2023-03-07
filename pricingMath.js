b60b6b9777a28c990e57341573a1a764d2ec6956
Whipstickgostop <8366326+Whipstickgostop@users.noreply.github.com>
2023-03-07 17:28:10 -0500

    scuffed update to support url params on load & on update, update page meta description

--- pricingMath.js
+++ pricingMath.js
@@ -3,9 +3,6 @@ function calculateTotal() {
   const minutes = document.getElementById("minutes").value;
   const seconds = document.getElementById("seconds").value;
 
-  // Calculate the total number of seconds
-  const totalSeconds = minutes * 60 + seconds;
-
   // Set the initial price per minute to 0
   let pricePerMinute = 0;
 
@@ -30,8 +27,58 @@ function calculateTotal() {
   const totalCost = (minutes * pricePerMinute) + (seconds * (pricePerMinute / 60));
 
   // Display the total cost
-  document.getElementById("total").innerHTML = totalCost.toFixed(2);
+  document.getElementById("total").innerHTML = `$${Math.ceil(totalCost)}`;
+
+  // Update meta description
+  document.head.querySelector('meta[name="description"]').content = `Total: $${Math.ceil(totalCost)}`;
+
+  // Update Query Params
+  const url = new URL(window.location.href);
+  url.searchParams.set('min', `${minutes}`);
+  url.searchParams.set('sec', `${seconds}`);
+  for (const key of ['expertPlus', 'expert', 'hard', 'mediumPlusEasy', 'lights']) {
+    if (document.getElementById(key).checked) {
+      url.searchParams.set(key, 1);
+    } else {
+      url.searchParams.delete(key)
+    }
+  }
+  window.history.replaceState('', '', url.href);
 }
+
+function prefillFormFromQuery() {
+  // grab values from url query
+  const params = new URLSearchParams(window.location.search);
+
+  // parse & apply values to inputs
+  document.getElementById('minutes').value = parseNumberParam(params.get('min')) || 4;
+  document.getElementById('seconds').value = parseNumberParam(params.get('sec') || 20);
+  document.getElementById('expertPlus').checked = parseBoolParam(params.get('expertPlus'));
+  document.getElementById('expert').checked = parseBoolParam(params.get('expert'));
+  document.getElementById('hard').checked = parseBoolParam(params.get('hard'));
+  document.getElementById('mediumPlusEasy').checked = parseBoolParam(params.get('mediumPlusEasy'));
+  document.getElementById('lights').checked = parseBoolParam(params.get('lights'));
+
+  // recalculate total
+  calculateTotal();
+}
+
+// numeric params
+function parseNumberParam(val) {
+  if (!val) return null;
+  val = Number(val);
+  return val && !isNaN(val) ? val : null;
+}
+
+// bool params as 1 or 0
+function parseBoolParam(val) {
+  if (!val) return false;
+  val = Number(val);
+  return val === 1;
+}
+
+// execute preill function on page load
+prefillFormFromQuery();
 
 // Add event listeners to the minutes and seconds input elements
 document.getElementById("minutes").addEventListener("input", calculateTotal);
